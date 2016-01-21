97f7141b3c234f3e0ef29c9a97d18fb8cb035c6d
vivekmannotra <vivekmannotra@rocketmail.com>
2016-01-22 02:29:55 +0530

    updates

--- dist/scripts/controllers/task.factory.js
+++
@@ -1,9 +0,0 @@
-angular.module('lexstartProjectApp').factory('taskFactory', taskFactory);
-taskFactory.$inject = ['$http'];
-function taskFactory($http){
-  return {
-    getTaskData: function() {
-      return $http.get('./task.json');
-    }
-  }
-}
